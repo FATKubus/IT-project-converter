ecbd1c84ca3029163724212fd1311432f7ab4f0d
FATKubus <135655354+FATKubus@users.noreply.github.com>
2023-06-07 12:36:24 +0200

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,1 +1,17 @@
+def XMLtoJS(x, y): 
+    print("Zrozumiano. [.xml to .json]")
+    print("Próba utworzenia nowego pliku...")
+    data = upload_xml(x)
+    return_json(y, data)
+    print("Sukces!")
 
+def YMLtoJS(x, y): 
+    print("Zrozumiano. [.yml to .json]")
+    print("Próba utworzenia nowego pliku...")
+    data = upload_yml(x)
+    return_json(y, data)
+    print("Sukces!")
+
+def return_json(x, data):
+    with open(x, "w") as f:
+        f.write(json.dumps(data)) 

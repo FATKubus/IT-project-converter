960131211dfd9225945421c5e3a4a76d1dcf5015
FATKubus <135655354+FATKubus@users.noreply.github.com>
2023-06-07 13:03:23 +0200

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -2,18 +2,16 @@ import os
 import json
 import yaml
 import xmltodict
-from dicttoxml import dicttoxml
+
 import argparse
 
-parser = argparse.ArgumentParser(description="Konwerter plików [.xml], [.json], oraz [.yml]")
-parser.add_argument('source_file', help='ścieżka pliku źródłowego (wraz z jego rozszerzeniem)')
-parser.add_argument('destination_file', help='ścieżka pliku docelowego (wraz z jego rozszerzeniem)')
 
+parser = argparse.ArgumentParser(description='Konwertuj pliki między formatami .xml, .json i .yml.')
+parser.add_argument('--source', type=str, help='Ścieżka pliku źródłowego (w tym jego rozszerzenie).')
+parser.add_argument('--dest', type=str, help='Ścieżka pliku docelowego i rozszerzenie, w którym chcesz go zapisać
+.')
 args = parser.parse_args()
 
-x = args.source_file
-y = args.destination_file
-
-file_name, file_extension = os.path.splitext(x)
-file_name_new, file_extension_new = os.path.splitext(y)
+a = args.source
+b = args.dest
 

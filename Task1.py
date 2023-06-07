a7f8325db80ba9a827eae712716d3b90b330e8e3
FATKubus <135655354+FATKubus@users.noreply.github.com>
2023-06-07 12:27:19 +0200

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,1 +1,19 @@
+import os
+import json
+import yaml
+import xmltodict
+from dicttoxml import dicttoxml
+import argparse
+
+parser = argparse.ArgumentParser(description="Konwerter plików [.xml], [.json], oraz [.yml]")
+parser.add_argument('source_file', help='ścieżka pliku źródłowego (wraz z jego rozszerzeniem)')
+parser.add_argument('destination_file', help='ścieżka pliku docelowego (wraz z jego rozszerzeniem)')
+
+args = parser.parse_args()
+
+x = args.source_file
+y = args.destination_file
+
+file_name, file_extension = os.path.splitext(x)
+file_name_new, file_extension_new = os.path.splitext(y)
 

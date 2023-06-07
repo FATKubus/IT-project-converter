3b2422cc75ecc4b0bf7afa94d9d3488b7310524e
FATKubus <135655354+FATKubus@users.noreply.github.com>
2023-06-07 13:15:56 +0200

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,1 +1,18 @@
+ def JStoYML(a, b):
+    print("Zrozumiano. [.json to .yml]")
+    print("Próba utworzenia nowgo pliku")
+    data = load_js(a)
+    write_yaml(b, data)
+    print("Wykonano!")
 
+ 
+def XMLtoYML(a, b):   
+    print("Zrozumiano. [.xml to .yml]")
+    print("Próba utworzenia nowgo pliku")
+    data = load_xml(a)
+    write_yaml(b, data)
+    print("Wykonano!")
+    
+def write_yaml(a, data):
+    with open(a, 'w') as f:
+        yaml.dump(data, f)
